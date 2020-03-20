68438df610b2e6e117fca42cfb0c721640253597
aendrevv <aendrevv@gmail.com>
2020-03-21 00:53:52 +0200

    Day 2

--- src/telegram/testBot.js
+++ src/telegram/testBot.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-plusplus */
+require('dotenv').config();
+
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '746353040:AAEI_tYU2rs9Ztzbq8sgC8qyYiIYoQcrYGY';
+const token = process.env.TESTTOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/test/, (msg) => {
   const userId = msg.from.id;
 
-  bot.sendMessage(userId, 'Coronavirus!');
+  bot.sendMessage(userId, 'Wake up, Neo. \nCoronavirus has you!!');
 });
