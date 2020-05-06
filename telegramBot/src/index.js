714a63610e4ec6d0dc24df437be5de91bff2a908
aendrevv <andrew.tolmachov@gmail.com>
2020-05-06 14:56:10 +0300

    current progress

--- telegramBot/src/index.js
+++ telegramBot/src/index.js
@@ -1,19 +1,33 @@
 const session = require('telegraf/session');
 const Router = require('telegraf/router');
 const bot = require('./bot');
-const { stage, stagesArray } = require('./scenes');
+const telegram = require('telegraf/telegram');
+const { Stage } = require('telegraf');
+const { newUser } = require('./scenes/newUser');
 const { startRegistration, mainMenu } = require('./menu');
+
+const stage = new Stage([newUser]);
+
 
 bot.use(session());
 bot.use(stage.middleware());
 
 bot.start(ctx => {
-  // if (!(await getUserId({ platformId: ctx.update.message.from.id, platformType: TELEGRAM }))) {
-  //   ctx.reply(WELCOME_MESSAGE, startRegistration);
-  //   return;
-  // }
-  ctx.reply(`Hello! You have joined 'Blood-Exchange-Bot ^__^'`, startRegistration);
-  // ctx.reply(ALREADY_REGISTRATED, mainMenu);
+  let user = ctx.update.message.from;
+  console.log(user);
+  // check if there in DB any user with this telegramID
+  // if (!(await db.User.findByTelegramId(ctx.update.message.from.id))) 
+  ctx.reply(`Hello ${user.first_name}! You have joined Blood-Exchange-Bot!`, ctx.scene.enter('new_user'));
+  // ctx.reply(`Wellcome back ${user.first_name}, please choose:\n`, mainMenu);
 });
 
+// bot.on('/start', Stage.enter('new_user'));
+
+bot.on('message', (msg) => {
+  console.log(msg);
+  bot.telegram.sendMessage(process.env.ADMIN, msg.chat.id, msg.message_id);
+  // bot.forwardMessage(process.env.ADMIN, msg.chat.id, msg.message_id);
+})
+
 bot.launch();
+
--- telegramBot/src/scenes/newUser.js
+++ telegramBot/src/scenes/newUser.js
@@ -1,67 +1,77 @@
-// new
-const WizardScene = require('telegraf/scenes/wizard');
-const Composer = require('telegraf/composer');
-const session = require('telegraf/session');
-const Stage = require('telegraf/stage');
 const Markup = require('telegraf/markup');
-const {
-  events: {
-    SCENES: { NEW_USER: name },
-  },
-} = require('../config');
-const { mainMenu, startRegistration } = require('../menu');
+const WizardScene = require('telegraf/scenes/wizard');
+const validator = require('validator');
 
-const stepHandler = new Composer();
-stepHandler.action('contact', ctx => {
-  ctx.reply(
-    `Натисніть кнопку "Відправити контакт", щоби ми могли з вами зв'язатись`,
-    {
-      reply_markup: {
-        keyboard: [
-          [{ text: '📱 Відправити контакт', request_contact: true }],
-          ['◀️ Назад'],
-        ],
-        resize_keyboard: true,
-        one_time_keyboard: true,
-      },
-    }
-  );
-  return ctx.wizard.next();
-});
-
-const scene = new WizardScene(
-  name,
+// new user registrator five-step wizard
+const newUser = new WizardScene(
+  'new_user',
   async ctx => {
-    try {
-      await ctx.reply(
-        `Натисніть кнопку "Відправити контакт", щоби ми могли з вами зв'язатись`,
-        {
-          reply_markup: {
-            keyboard: [
-              [{ text: '📱 Відправити контакт', request_contact: true }],
-              ['◀️ Назад'],
-            ],
-            resize_keyboard: true,
-            one_time_keyboard: true,
-          },
-        }
-      );
-      console.log(contact);
-      console.log(ctx.message.contact.phone_number);
-      stepHandler,
-      ctx => {
-        ctx.reply('Вітаю! Тепер Ви зареєстрований користувач BloodExchangeBot!', mainMenu);
-        return ctx.scene.leave();
-      }
-    } catch (error) {
-      ctx.reply('An error was occured(\nPlease try again later!', startRegistration);
-    }
-    return ctx.scene.leave();
+    ctx.reply('Send me your number please', { 
+      reply_markup: { 
+        keyboard: [[{text: '📲 Send phone number', request_contact: true}]],
+        resize_keyboard: true,
+        one_time_keyboard: true, 
+      },
+    });
+    ctx.wizard.state.contact = ctx.message.contact;
+    console.log(ctx.wizard.state.contact);
+    return ctx.wizard.next();
+  },
+  ctx => {
+    ctx.reply("Please, tell me your FULL name"); 
+    // enter your name
+    return ctx.wizard.next();
+  },
+  (ctx) => {
+    // validation example
+    if (ctx.message.text.length < 2) {
+      ctx.reply('Please enter your real name');
+      return ctx.wizard.prev();
+    } else return ctx.wizard.next();
+  },  
+  ctx => {
+    ctx.wizard.state.fullName = ctx.message.text; // store fullName in the state to share data between middlewares
+    ctx.reply(`Please enter your email:`);
+    return ctx.wizard.next();
+  },
+  ctx => {
+    console.log(ctx.message.text);
+    if (validator.isEmail(ctx.message.text)) {
+      ctx.wizard.state.email = ctx.message.text;    
+      ctx.reply(`Enter your DOB:`, Markup.inlineKeyboard([
+        Markup.callbackButton(`I don't know`, () => ctx.wizard.state.dob = '01011990'),
+      ]).extra());
+      return ctx.wizard.next();
+    } else return ctx.wizard.prev();
+  },
+  ctx => {
+    ctx.wizard.state.email = ctx.message.text; // store fullName in the state to share data between middlewares
+    console.log(ctx.wizard.state.email);
+    
+    ctx.reply(`Please enter your date of birth:`);
+    return ctx.wizard.next();
+  },
+  ctx => {
+    console.log(ctx.message.text);
+    if (validator.toDate(ctx.message.text)) {
+      ctx.wizard.state.dob = ctx.message.text;    
+      ctx.reply(`Please choose your blood type:`, Markup.inlineKeyboard([
+        Markup.callbackButton('1', () => ctx.wizard.state.bloodType = 1),
+        Markup.callbackButton('2', () => ctx.wizard.state.bloodType = 2),
+        Markup.callbackButton('3', () => ctx.wizard.state.bloodType = 3),
+        Markup.callbackButton('4', () => ctx.wizard.state.bloodType = 4),
+        Markup.callbackButton(`I don't know`, () => ctx.wizard.state.bloodType = undefined),
+      ]).extra());
+      return ctx.wizard.next();
+    } else return ctx.wizard.prev();
+  },
+  ctx => {
+    // ctx.replyWithDice();
+    // 
+    return ctx.wizard.leave();
   },
 );
 
 module.exports = {
-  name,
-  scene,
+  newUser,
 };
-
