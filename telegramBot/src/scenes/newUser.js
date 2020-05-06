7822baf620403bd69cb51455d33b99cc8cf3e331
aendrevv <andrew.tolmachov@gmail.com>
2020-05-06 16:30:45 +0300

    current progress

--- telegramBot/src/scenes/newUser.js
+++ telegramBot/src/scenes/newUser.js
@@ -28,25 +28,20 @@ const newUser = new WizardScene(
   ctx => {
     console.log(ctx.message.text);
     if (validator.isEmail(ctx.message.text)) {
-      ctx.wizard.state.email = ctx.message.text;    
-      ctx.reply(`Enter your DOB:`, Markup.inlineKeyboard([
-        Markup.callbackButton(`I don't know`, () => ctx.wizard.state.dob = '01011990'),
-      ]).extra());
-      return ctx.wizard.next();
-    } else return ctx.wizard.prev();
-  },
-  ctx => {
-    ctx.wizard.state.email = ctx.message.text; // store fullName in the state
-    console.log(ctx.wizard.state.email);
-    
-    ctx.reply(`Please enter your date of birth:`);
+      ctx.wizard.state.email = ctx.message.text;  
+      ctx.reply(`Please enter your date of birth:`);
+    } else {
+      ctx.reply(`Not valid email, but nevermind :) U can fix it later. Please enter your date of birth:`);
+
+    }
     return ctx.wizard.next();
   },
+
   ctx => {
     console.log(ctx.message.text);
-    ctx.wizard.state.dob = validator.toDate(ctx.message.text);
+    // ctx.wizard.state.dob = validator.toDate(ctx.message.text);
     console.log(ctx.wizard.state.dob);
-    ctx.reply(`Please choose your blood type:`, Markup.inlineKeyboard([
+    ctx.reply(`Please choose your blood type:`, Markup.keyboard([
       Markup.callbackButton('1', () => ctx.wizard.state.bloodType = 1),
       Markup.callbackButton('2', () => ctx.wizard.state.bloodType = 2),
       Markup.callbackButton('3', () => ctx.wizard.state.bloodType = 3),
@@ -57,10 +52,10 @@ const newUser = new WizardScene(
   },
   ctx => {
     console.log(ctx.wizard.state.bloodType);
-    ctx.reply(`Please choose your rhesus:`, Markup.inlineKeyboard([
+    ctx.reply(`Please choose your rhesus:`, Markup.keyboard([
       Markup.callbackButton('+', () => ctx.wizard.state.bloodType = '+'),
       Markup.callbackButton('-', () => ctx.wizard.state.bloodType = '-'),
-      Markup.callbackButton(`I don't care:`, () => ctx.wizard.state.bloodType = undefined),
+      Markup.callbackButton(`I don't know`, () => ctx.wizard.state.bloodType = undefined),
     ]).extra());
     return ctx.wizard.next();
   },  
